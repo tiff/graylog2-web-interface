9fa65ee953b441926e4666b07c6742a8528a6246
Lennart Koopmann <lennart@socketfeed.com>
2011-08-04 15:08:18 +0200

    fix broken tabs in #events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,11 +2,13 @@ module EventsHelper
 
   def event_tabs
     tabs = Array.new
-    tabs.push ["Show", events_event_path(@event_description)] if permitted_to?(:events, @event_description)
-    tabs.push ["Rules", rules_event_path(@event_description)] if permitted_to?(:rules, @event_description)
-    tabs.push ["Settings", settings_event_path(@event_description)] if permitted_to?(:settings, @event_description)
+    
+    if @event
+    tabs.push ["Show", events_event_path(@event)] if permitted_to?(:events, :events)
+    tabs.push ["Rules", rules_event_path(@event)] if permitted_to?(:rules, :events)
+    tabs.push ["Settings", settings_event_path(@event)] if permitted_to?(:settings, :events)
+    end
 
     return tabs
   end
-
 end
