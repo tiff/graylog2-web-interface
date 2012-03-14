4d6ed6e5bec1c863ad13af3d81c0b8416a97dbb0
Chris Lowder <clowder@gmail.com>
2012-03-14 14:25:27 +0000

    A far more complicated set of LDAP config options that should hopefully cover more use cases.

--- app/concerns/ldap_authentication.rb
+++ app/concerns/ldap_authentication.rb
@@ -1,14 +1,14 @@
 module LdapAuthentication
   def self.extended(base_class)
     base_class.extend(LdapAuthentication::ClassMethods)
-    base_class.send(:include, LdapAuthentication::InstanceMethods)
-
     base_class.class_eval do
       class << self
         alias_method_chain :authenticate, :ldap
       end
 
       field :from_ldap, :type => Boolean, :default => false
+
+      attr_accessible :from_ldap
     end
   end
 
@@ -22,8 +22,12 @@ module LdapAuthentication
                                                      :name                  => authenticated_user.displayname.first,
                                                      :password              => password,
                                                      :password_confirmation => password,
-                                                     :role                  => User::STANDARD_ROLE })
-        user.update_attribute(:from_ldap, true)
+                                                     :role                  => User::STANDARD_ROLE,
+                                                     :from_ldap             => true })
+
+        # Use the most recent information from LDAP
+        user.update_attributes!(:email => authenticated_user.mail.first,
+                                :name  => authenticated_user.displayname.first)
         user
       else
         authenticate_without_ldap(login, password)
@@ -32,31 +36,40 @@ module LdapAuthentication
 
     private
     # Returns a single Net::LDAP::Entry or false
-    def ldap_login(login, password)
-      ldap_session       = new_ldap_session(login, password)
-      authenticated_user = ldap_session.bind_as({ :base     => ::Configuration.ldap_base,
-                                                  :filter   => "(#{::Configuration.ldap_username_attr}=#{ login })",
-                                                  :password => password })
+    def ldap_login(username, password)
+      ldap_session       = new_ldap_session
+      bind_args          = args_for(username, password)
+      authenticated_user = ldap_session.bind_as(bind_args)
 
       authenticated_user ? authenticated_user.first : false
     end
 
-    def new_ldap_session(login, password)
+    # This is where LDAP jumps up and punches you in the face, all the while
+    # screaming "You never gunna get this, your wasting your time!".
+    def args_for(username, password)
+      user_filter = "#{ ::Configuration.ldap_username_attribute }=#{ username }"
+      args        = { :base     => ::Configuration.ldap_base,
+                      :filter   => "(#{ user_filter })",
+                      :password => password }
+
+      unless ::Configuration.ldap_can_search_anonymously?
+        # If you can't search your LDAP directory anonymously we'll try and
+        # authenticate you with your user dn before we try and search for your
+        # account (dn example. `uid=clowder,ou=People,dc=mycompany,dc=com`).
+        user_dn = [user_filter, ::Configuration.ldap_base].join(',')
+        args.merge({ :auth => { :username => user_dn, :password => password, :method => :simple } })
+      end
+
+      args
+    end
+
+    def new_ldap_session
       Net::LDAP.new(:host       => ::Configuration.ldap_host,
                     :port       => ::Configuration.ldap_port,
                     :encryption => ::Configuration.ldap_encryption.try(:to_sym),
-                    :base       => ::Configuration.ldap_base,
-                    :auth => {
-                        :username => "#{::Configuration.ldap_bind_domain_name}\\#{login}",
-                        :password => password,
-                        :method => :simple
-                      }
-                    )
+                    :base       => ::Configuration.ldap_base)
     end
 
-  end
-
-  module InstanceMethods
   end
 
   module Configuration
@@ -84,12 +97,12 @@ module LdapAuthentication
       ldap_config :encryption
     end
 
-    def ldap_username_attr
-      ldap_config(:username_attr) || "uid"
+    def ldap_username_attribute
+      ldap_config :username_attribute, 'uid'
     end
 
-    def ldap_bind_domain_name
-      ldap_config :bind_domain_name
+    def ldap_can_search_anonymously?
+      ldap_config :can_search_anonymously, true
     end
   end
 end
